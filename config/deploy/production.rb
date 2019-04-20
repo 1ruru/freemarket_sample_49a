2d70850999e840ae669c3e8bf762f1b2b42398b9
Seiji Sakai <iruru147@icloud.com>
2019-04-20 17:44:06 +0900

    update change deploy setting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,5 +60,11 @@
 #     # password: "please use keys"
 #   }
 server '18.182.139.35',
-  user: 'iruru',
+  user: 'ec2-user',
   roles: %w{app db web}
+  ssh_options: {
+       port: 22,
+       user: "ec2-user",
+       keys: %w(~/.ssh/freemarcket_key_pair.pem),
+       forward_agent: true
+   }
