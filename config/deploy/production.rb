bf3571dcceb2a144d50ae908f2adf423aeebbe21
Seiji Sakai <iruru147@icloud.com>
2019-04-20 18:58:26 +0900

    update cap setting

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,10 @@
 server '18.182.139.35',
   user: 'ec2-user',
-  roles: %w{app db web}
-  # ,ssh_options: {
-  #      port: 22,
-  #      user: "ec2-user",
-  #      keys: %w(~/.ssh/freemarcket_key_pair.pem),
-  #      forward_agent: true
-  #  }
+  roles: %w{app db web},
+  ssh_options: {
+       port: 22,
+       user: "ec2-user",
+       keys: %w(~/.ssh/freemarcket_key_pair.pem),
+       auth_methods: %w{publickey},
+       forward_agent: true
+   }
