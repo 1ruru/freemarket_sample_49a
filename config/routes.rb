264ff37bdf8d98a77128249e3d3c2cfe6e6d1a5c
Seiji Sakai <iruru147@icloud.com>
2019-04-21 20:55:52 +0900

    update rooting for sign-up redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'items#index'
-  resources :users
+  resources :users do
+    get 'sign_up_menu', on: :collection
+  end
   resources :items
 end
