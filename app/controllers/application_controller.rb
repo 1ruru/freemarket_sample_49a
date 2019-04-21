03459f22c5a03dd4ca7b2863af552598255811e4
Seiji Sakai <iruru147@icloud.com>
2019-04-21 20:52:57 +0900

    add devise users controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:index, :search]
+  before_action :authenticate_user!, except: [:index, :search, :sign_up_menu]
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth, if: :production?
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :family_name_kana, :first_name, :first_name_kana, :birthday, :phone])
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,5 @@
 class UsersController < ApplicationController
+  def sign_up_menu
+    
+  end
 end
